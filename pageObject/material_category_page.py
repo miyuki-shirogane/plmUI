f8d50cf513df4a2ada9198ca2b8a89929567a661
miyuki-shirogane <yilin@feiyilin-MacBook.local>
2021-12-16 18:44:40 +0800

    少量更新

--- pageObject/material_category_page.py
+++ pageObject/material_category_page.py
@@ -1,23 +1,24 @@
 import time
+from random import randint
 from selenium.webdriver.common.by import By
 from pageObject.base_page import BasePage
-from pageObject.material_manage_page import MaterialManagePage
 from utils.mock import Mock
 
 
 class MaterialCategoryPage(BasePage):
     _base_url = "https://comba-test.teletraan.io/subapp/plm/base/category"
 
-    def create_material_category(self, pick_num_category_form=1):
+    def create_material_category_get_name(self, pick_num_category_form=randint(1, 4)):
         mock = Mock()
-        category = mock.mock_data('category')
+        category_name = mock.mock_data('category_name')
         self.driver.find_element(By.XPATH, '//button[span="新增物料类别"]').click()
         self.driver.find_element(By.XPATH, '//div[label="*物料类别"]/ancestor::div//input[@name="property"]').click()
         self.driver.find_element(By.XPATH, f'//div[@class="MuiAutocomplete-popper"]//li[{pick_num_category_form}]')\
             .click()
-        self.driver.find_element(By.XPATH, '//input@[name="name"]').send_keys(category)
+        self.driver.find_element(By.XPATH, '//input@[name="name"]').send_keys(category_name)
         self.driver.find_element(By.XPATH, '//button[span="确定"]')
-        return MaterialManagePage(self.driver)
+        time.sleep(1)
+        return category_name
 
     # filter="物料属性"，list搜索，获取返回物料类别列表
     def get_materials_category_search(self, pick_num_category_filter: int):
--- testcase/test_material_category.py
+++ testcase/test_material_category.py
@@ -4,3 +4,9 @@ from pageObject.material_category_page import MaterialCategoryPage
 class TestCreateMaterialCategory:
     def setup_class(self):
         self.mat_cg = MaterialCategoryPage()
+
+    def test(self):
+        pass
+
+    def teardown(self):
+        self.mat_cg.driver.quit()
